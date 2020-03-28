1524ab93b55adefeada55656ae0debe0ffb8d0b6
rezkitr <rtriandy@gmail.com>
2020-03-28 18:47:16 +0700

    fix banner responsive

--- client/src/components/PageBanner.js
+++ client/src/components/PageBanner.js
@@ -5,8 +5,8 @@ function PageBanner(props) {
   return (
     <div className="container-fluid page-banner">
       <div className="row justify-content-center">
-        <div className="col-md-4 text-right my-auto">
-          <img src={props.img} alt="illustration" width="500px" />
+        <div className="col-md-4 my-auto">
+          <img src={props.img} alt="illustration" />
         </div>
         <div className="col-md-4 my-auto">
           <h1 className="page-banner-title mb-4">{props.title}</h1>
