1fc3110d7bee2c0e5e53fe99608c1055e78ba150
rezkitr <rtriandy@gmail.com>
2020-03-28 13:00:13 +0700

    change gallery section design

--- client/src/components/GallerySection.js
+++ client/src/components/GallerySection.js
@@ -1,36 +1,48 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import Gallery from 'react-grid-gallery'
-import photos from '../photoSection'
+import React from "react";
+import { Link } from "react-router-dom";
+import Gallery from "react-grid-gallery";
 
-import SectionTitle from './SectionTitle'
+import photos from "../photoSection";
+import bgImage from "../assets/bg-gallery.jpg";
 
-class GallerySection extends Component {
-
-  state = {
-    photos : []
-  }
-
-  componentDidMount () {
-    this.setState({
-      photos : photos
-    })
-  }
-
-  render() {
-    return (
-      <div className="container-fluid gal-section">
-        <SectionTitle secTitle="Galeri" />
-        <div className="row mx-3">
-          <div className="col-md">
-            <Gallery enableLightbox={false} images={this.state.photos} enableImageSelection={false} rowHeight={240} maxRows={2} margin={2} backdropClosesModal={true} />
-          </div>
+function GallerySection() {
+  return (
+    <div
+      className="container-fluid gal-section"
+      style={{
+        backgroundImage: `url(${bgImage})`,
+        backgroundSize: "cover",
+        backgroundPositionY: "-180px"
+      }}
+    >
+      <div className="row p-0">
+        <div className="col-md-8 p-5">
+          <Gallery
+            enableLightbox={false}
+            images={photos}
+            enableImageSelection={false}
+            rowHeight={240}
+            maxRows={2}
+            margin={2}
+            backdropClosesModal={true}
+          />
         </div>
-        <div className="row justify-content-center mt-5">
-        <Link to="/galeri"><button className="btn btn-light-green font-weight-bold">LAINNYA</button></Link>
+        <div className="col-md text-center my-auto p-5">
+          <i className="fas fa-quote-right fa-3x"></i>
+          <p className="mt-4 mb-0" style={{ fontSize: "28px" }}>
+            A good snapshot keeps a moment from running away.
+          </p>
+          <small className="mt-0">- E. Welty</small>
+          <br />
+          <Link to="/galeri">
+            <button className="btn btn-sm btn-outline-dark mt-4">
+              LAINNYA
+            </button>
+          </Link>
+        </div>
       </div>
-      </div>
-    )
-  }
+    </div>
+  );
 }
-export default GallerySection
+
+export default GallerySection;
