4ec3655fde32fd404cb76746e6d9a5a107ea0496
rezkitr <rtriandy@gmail.com>
2020-04-02 14:58:24 +0700

    Update AdminAdd.js

--- client/src/adminpage/AdminAdd.js
+++ client/src/adminpage/AdminAdd.js
@@ -3,12 +3,6 @@ import React from "react";
 import UserAdminAddForm from "../admincomponent/UserAdminAddForm";
 
 function AdminAdd(props) {
-  if (
-    window.localStorage.getItem("isLoggedIn") === "false" ||
-    window.localStorage.getItem("isLoggedIn") === null
-  ) {
-    props.history.push("/admin/login");
-  }
   return (
     <div>
       <UserAdminAddForm {...props} />
