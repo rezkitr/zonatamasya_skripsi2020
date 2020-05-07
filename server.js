8275c4a3056ed00ebb8de01c618acb0a0bcbc72d
rezkitr <rtriandy@gmail.com>
2020-05-07 13:00:38 +0700

    Update server.js

--- server.js
+++ server.js
@@ -51,6 +51,7 @@ app.use("/photo", photoRouter);
 // serve static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.use(express.static("client/public"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
