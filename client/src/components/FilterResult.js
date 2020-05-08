d62cfb7c5fa7d8caecb777fc8947709e3a3d254b
rezkitr <rtriandy@gmail.com>
2020-05-08 12:21:21 +0700

    update filter

--- client/src/components/FilterResult.js
+++ client/src/components/FilterResult.js
@@ -4,32 +4,47 @@ import OpenTripData from "../tripDataSource";
 import TripCard from "./TripCard";
 
 function FilterResult(props) {
+  let resultCount = false;
+
+  const FilteredTripItem = props.tripData.map((item) => {
+    if (props.tag === item.region) {
+      resultCount = true;
+      return (
+        <div className="col my-3">
+          <TripCard
+            key={item._id}
+            tripId={item._id}
+            cardImg={item.cardImage}
+            name={item.name}
+            price={item.price.priceFull}
+            start={item.departure.start}
+          />
+        </div>
+      );
+    }
+  });
+
   if (props.reset === false) {
     return (
       <div className="container-fluid trip-card">
-        <div className="row row-cols-md-2 row-cols-lg-5 mx-3 mt-3">
-          {props.tripData.map(item =>
-            props.tag === item.region ? (
-              <div className="col my-3">
-                <TripCard
-                  key={item._id}
-                  tripId={item._id}
-                  cardImg={item.cardImage}
-                  name={item.name}
-                  price={item.price.priceFull}
-                  start={item.departure.start}
-                />
-              </div>
-            ) : null
-          )}
-        </div>
+        {resultCount ? (
+          <div className="row row-cols-md-2 row-cols-lg-5 mx-3 mt-3">
+            {FilteredTripItem}
+          </div>
+        ) : (
+          <div className="row">
+            <div className="col text-center ">
+              <h3 className="text-danger">Belum Tersedia</h3>
+            </div>
+          </div>
+        )}
       </div>
     );
   } else {
     return (
       <div className="container-fluid trip-card">
         <div className="row row-cols-md-2 row-cols-lg-5 mx-3 mt-3">
-          {props.tripData.map(item => (
+          {props.tripData.map((item) => (
             <div className="col my-3">
               <TripCard
                 key={item._id}
